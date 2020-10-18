578fe4b5a183487fb174cf9e3aeae2b074984d3f
Seaborn Lee <seabornlee.cn@gmail.com>
2020-10-18 15:51:58 +0800

    Make it even worse

--- src/Length.cpp
+++ src/Length.cpp
@@ -1,7 +1,7 @@
 #include <cstring>
 #include "Length.h"
 
-Length::Length(double val, const char* uinnt) {
+Length::Length(double val, const char *uinnt) {
     this->val = val;
     this->uinnt = uinnt;
 }
@@ -15,30 +15,23 @@ const char *Length::getUinnt() {
 }
 
 Length Length::as(const char *u) {
-    Length len =  *this;
-    if (strcmp(this->uinnt, "f") == 0) {
-        if (strcmp(u, "yard") == 0) {
-            len = Length(this->val / 3, u);
-        } else if (strcmp(u, "inch") == 0) {
-            len = Length(this->val * 12, u);
-        }
-    }
+    Length len = *this;
+    if (strcmp(this->uinnt, "f") == 0 && strcmp(u, "inch") == 0) len = Length(this->val * 12, u);
+    if (strcmp(this->uinnt, "inch") == 0 && strcmp(u, "f") == 0) len = Length(this->val / 12, u);
 
-    if (strcmp(this->uinnt, "yard") == 0) {
-        if (strcmp(u, "inch") == 0) {
-            len = Length(this->val * 36, u);
-        } else if (strcmp(u, "f") == 0) {
-            len = Length(this->val * 3, u);
-        }
-    }
-
-    if (strcmp(this->uinnt, "inch") == 0) {
-        if (strcmp(u, "f") == 0) {
-            len = Length(this->val / 12, u);
+        if (strcmp(this->uinnt, "yard") == 0) {
+            if (strcmp(u, "inch") == 0) {
+                len = Length(this->val * 36, u);
+            } else if (strcmp(u, "f") == 0) {
+                len = Length(this->val * 3, u);
+            }
         } else if (strcmp(u, "yard") == 0) {
-            len = Length(this->val / 36, u);
+            if (strcmp(this->uinnt, "f") == 0) {
+                len = Length(this->val / 3, u);
+            } else if (strcmp(this->uinnt, "inch") == 0) {
+                len = Length(this->val / 36, u);
+            }
         }
-    }
     return len;
 }
 
