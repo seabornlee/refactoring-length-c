a312c3db543e82e4e110a39ac62a54b2fee9c979
Seaborn Lee <seabornlee.cn@gmail.com>
2020-10-23 13:46:47 +0800

    1 import project

--- src/length.c
+++ src/length.c
@@ -11,22 +11,27 @@ Length *newLength(double val, const char *uinnt) {
 
 LengthPtr as(LengthPtr obj, const char *u) {
     LengthPtr len = obj;
-    if (strcmp(obj->uinnt, "f") == 0 && strcmp(u, "inch") == 0) len = newLength(obj->val * 12, u);
-    if (strcmp(obj->uinnt, "inch") == 0 && strcmp(u, "f") == 0) len = newLength(obj->val / 12, u);
+    if (strcmp(obj->uinnt, "f") == 0 && strcmp(u, "inch") == 0) {
+        len = newLength(obj->val * 12, u);
+    }
 
-        if (strcmp(obj->uinnt, "yard") == 0) {
-            if (strcmp(u, "inch") == 0) {
-                len = newLength(obj->val * 36, u);
-            } else if (strcmp(u, "f") == 0) {
-                len = newLength(obj->val * 3, u);
-            }
-        } else if (strcmp(u, "yard") == 0) {
-            if (strcmp(obj->uinnt, "f") == 0) {
-                len = newLength(obj->val / 3, u);
-            } else if (strcmp(obj->uinnt, "inch") == 0) {
-                len = newLength(obj->val / 36, u);
-            }
+    if (strcmp(obj->uinnt, "inch") == 0 && strcmp(u, "f") == 0) {
+        len = newLength(obj->val / 12, u);
+    }
+
+    if (strcmp(obj->uinnt, "yard") == 0) {
+        if (strcmp(u, "inch") == 0) {
+            len = newLength(obj->val * 36, u);
+        } else if (strcmp(u, "f") == 0) {
+            len = newLength(obj->val * 3, u);
         }
+    } else if (strcmp(u, "yard") == 0) {
+        if (strcmp(obj->uinnt, "f") == 0) {
+            len = newLength(obj->val / 3, u);
+        } else if (strcmp(obj->uinnt, "inch") == 0) {
+            len = newLength(obj->val / 36, u);
+        }
+    }
     return len;
 }
 
